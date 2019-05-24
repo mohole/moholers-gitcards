05694f410085ce5ac119c9f24b5e173e926f3129
Salvatore <salvatore@salvatores-mbp.lan>
2019-05-24 21:03:21 +0200

    Updates

---
+++ precache-manifest.f15efbb6e4a0f29051571029787643a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c628ef8cc338d35539dfcae9b96f283e",
+    "url": "/moholers-gitcards/index.html"
+  },
+  {
+    "revision": "f6d722584414ef6a715d",
+    "url": "/moholers-gitcards/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "020d0a404b761468034b",
+    "url": "/moholers-gitcards/static/css/main.b6e57b03.chunk.css"
+  },
+  {
+    "revision": "f6d722584414ef6a715d",
+    "url": "/moholers-gitcards/static/js/2.db4a4140.chunk.js"
+  },
+  {
+    "revision": "020d0a404b761468034b",
+    "url": "/moholers-gitcards/static/js/main.e7a9495e.chunk.js"
+  },
+  {
+    "revision": "dbfaaadbd218139bc0be",
+    "url": "/moholers-gitcards/static/js/runtime~main.81189d9b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.65e390abf0a3a6e46c4e08451084f59a.js"
+  "/moholers-gitcards/precache-manifest.f15efbb6e4a0f29051571029787643a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moholers-gitcards/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
